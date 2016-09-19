6aa5d71876d10e475db073cff40d49a09d5aca29
lagunacomputer <lagunacomputer@gmail.com>
2016-09-18 21:54:02 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+var ds18b20 = require('ds18b20');
+var Service, Characteristic;
+
+module.exports = function(homebridge) {
+  Service = homebridge.hap.Service;
+  Characteristic = homebridge.hap.Characteristic;
+  homebridge.registerAccessory("homebridge-temperature-ds18b20", "PhotoCell", TemperatureAccessory);
+}
+
+function TemperatureAccessory(log, config) {
+  this.log = log;
+  this.name = config["name"];
+  this.device = config["device"];
+
+  this.service = new Service.TemperatureSensor(this.name);
+
+  this.service
+    .getCharacteristic(Characteristic.CurrentAmbientLightLevel)
+    .setProps({ minValue: -55, maxValue: 125 })
+    .on('get', this.getState.bind(this));
+}
+
+TemperatureAccessory.prototype.getState = function(callback) {
+  ds18b20.temperature(this.device, function(err,value){
+    callback(err, value);
+  });
+}
+
+TemperatureAccessory.prototype.getServices = function() {
+  return [this.service];
+}
