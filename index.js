28899544503e703f17ff58baba0f7ca4e5197648
lagunacomputer <lagunacomputer@gmail.com>
2016-09-18 22:18:08 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-var ds18b20 = require('ds18b20');
+
+var ds18b20 = require('ds18b20');
 var Service, Characteristic;
 
 module.exports = function(homebridge) {
   Service = homebridge.hap.Service;
   Characteristic = homebridge.hap.Characteristic;
-  homebridge.registerAccessory("homebridge-temperature-ds18b20", "PhotoCell", TemperatureAccessory);
+  homebridge.registerAccessory("homebridge-CurrentAmbientLightLevel", "PhotoCell", LightSensorAccessory);
 }
 
-function TemperatureAccessory(log, config) {
+function LightSensorAccessory(log, config) {
   this.log = log;
   this.name = config["name"];
   this.device = config["device"];
 
-  this.service = new Service.TemperatureSensor(this.name);
+  this.service = new Service.LightSensor(this.name);
 
   this.service
     .getCharacteristic(Characteristic.CurrentAmbientLightLevel)
@@ -20,12 +21,46 @@ function TemperatureAccessory(log, config) {
     .on('get', this.getState.bind(this));
 }
 
-TemperatureAccessory.prototype.getState = function(callback) {
-  ds18b20.temperature(this.device, function(err,value){
-    callback(err, value);
-  });
+// LightSensorAccessory.prototype.getState = function(callback) {
+//  ds18b20.temperature(this.device, function(err,value){
+//    callback(err, value);
+//  });
+// }
+
+
+
+
+
+LightSensorAccessory.prototype.getState = function(callback) {
+  this.log("Getting current state...");
+  
+  request.get({
+    url: "http://192.168.1.201/?light",
+    qs: { access_token: this.accessToken }
+  }, function(err, response, body) {
+    
+    if (!err && response.statusCode == 200) {
+      var json = JSON.parse(body);
+      var state = json.lightval; // change temp or humidty to lightval in arduino
+      var locked = state == "lock"
+      callback(err, value) // success
+    }
+    else {
+      this.log("Error getting state (status code %s): %s", response.statusCode, err);
+      callback(err);
+    }
+  }.bind(this));
 }
 
-TemperatureAccessory.prototype.getServices = function() {
+
+
+
+
+
+
+
+
+
+LightSensorAccessory.prototype.getServices = function() {
   return [this.service];
 }
