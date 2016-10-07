808a1bcc17ea4094f8c6b415c59704c7c61d8618
lagunacomputer <lagunacomputer@gmail.com>
2016-10-07 16:58:20 -0700

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function Light(log, config) {
    this.url = config["url"];
    this.http_method = config["http_method"] || "GET";
    this.name = config["name"];
-   this.manufacturer = config["manufacturer"] || "@metbosch manufacturer";
+   this.manufacturer = config["manufacturer"] || "@lagunacomputer manufacturer";
    this.model = config["model"] || "Model not available";
    this.serial = config["serial"] || "Non-defined serial";
 }
